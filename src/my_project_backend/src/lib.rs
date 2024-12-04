03bbbeaac5288b41c719b0921f326b82d422648f
ritsu Rye <ritasabinat@gmail.com>
2024-12-04 11:17:21 +0000

    czat

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,24 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
+}
+
+#[ic_cdk::update]
+fn add_msg(new_msg: String) {
+    
+    CHAT.with(|chat| {
+        chat.borrow_mut().push(new_msg)
+    })
+}
+
+#[ic_cdk::query]
+fn get_chat() -> Vec<String> {
+    CHAT.with(|chat| chat.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
